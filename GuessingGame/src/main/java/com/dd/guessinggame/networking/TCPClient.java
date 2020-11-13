3cf4b9fa01aa1e8cba5ecc015df54eb9daaddf51
d471061c <cpastethat@gmail.com>
2020-11-13 17:14:13 +0200

    add tcp client

---
+++ GuessingGame/src/main/java/com/dd/guessinggame/networking/TCPClient.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dd.guessinggame.networking;
+
+import com.dd.guessinggame.networking.utils.Handler;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+/**
+ *
+ * @author d471061c
+ */
+public class TCPClient {
+    
+    private Socket socket;
+    private Handler handler;
+
+    
+    public boolean connect(String ipAddress, int port) throws IOException {
+        this.socket = new Socket(ipAddress, port);
+        this.handler = new Handler(this.socket);
+        return true;
+    }
+ 
+    
+    public void mainloop() {
+        Scanner scan = new Scanner(System.in);
+        String message, output;
+        
+        // Handle the communication
+        while (true) {
+            message = scan.nextLine();
+            
+            if (message.equals("quit")) {
+                break;
+            }
+            
+            try {
+                handler.send(message);
+                output = handler.readLine();
+                System.out.println("From server: '" + output + "'");
+            } catch (IOException ex) {
+                System.err.println("Error" + ex.getMessage());
+            }
+        }
+        
+        try {
+            this.socket.close();
+        } catch (IOException ex) {
+            System.err.println("Failed to shutdown the client: " + ex.getMessage());
+        }
+        
+        System.out.println("Bye!");
+    }
+}
